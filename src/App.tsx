a1ec854d5e4f7b99c2a35d06356bd70b278ce0e7
AhmedBedeir <bederahmed0@gmail.com>
2023-11-03 23:49:24 +0200

    add basename

--- src/App.tsx
+++ src/App.tsx
@@ -6,38 +6,43 @@ import Layout from "./components/Layout";
 import "./styles/global.scss";
 import Product from "./pages/product/Product";
 import User from "./pages/user/User";
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "users",
+          element: <Users />,
+        },
+        {
+          path: "user/:id",
+          element: <User />,
+        },
+        {
+          path: "products",
+          element: <Products />,
+        },
+        {
+          path: "product/:id",
+          element: <Product />,
+        },
+        {
+          path: "*",
+          element: <h1>Coming Soon ...</h1>,
+        },
+      ],
+    },
+  ],
   {
-    path: "/",
-    element: <Layout />,
-    children: [
-      {
-        path: "/",
-        element: <Home />,
-      },
-      {
-        path: "users",
-        element: <Users />,
-      },
-      {
-        path: "user/:id",
-        element: <User />,
-      },
-      {
-        path: "products",
-        element: <Products />,
-      },
-      {
-        path: "product/:id",
-        element: <Product />,
-      },
-      {
-        path: "*",
-        element: <h1>Coming Soon ...</h1>,
-      },
-    ],
-  },
-]);
+    basename: "/admin-dashboard/",
+  }
+);
 function App() {
   return <RouterProvider router={router} />;
 }
